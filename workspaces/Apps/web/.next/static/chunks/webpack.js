054526d3db5107ad6d16759d39c923af3a56f7c0
Branden Dane <danetheory@gmail.com>
2020-11-18 17:13:33 -0500

    first commit

--- workspaces/Apps/web/.next/static/chunks/webpack.js
+++ workspaces/Apps/web/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29316ccbab298f181d98";
+/******/ 	var hotCurrentHash = "1c89424b9a5680e3597c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
